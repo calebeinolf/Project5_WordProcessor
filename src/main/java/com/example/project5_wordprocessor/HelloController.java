e915c16f9d15d53a0e9e9e1a5a0d36f27d841181
calebeinolf <einolfcb22@gcc.edu>
2023-04-20 14:20:06 -0400

    Initial commit

---
+++ src/main/java/com/example/project5_wordprocessor/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.project5_wordprocessor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
