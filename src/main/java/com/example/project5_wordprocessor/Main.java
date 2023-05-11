fcd8d02face5fd2675238923d24e5d5056ad5309
calebeinolf <einolfcb22@gcc.edu>
2023-05-10 23:12:16 -0400

    final (?)

--- src/main/java/com/example/project5_wordprocessor/Main.java
+++ src/main/java/com/example/project5_wordprocessor/Main.java
@@ -3,6 +3,9 @@ package com.example.project5_wordprocessor;
 import javafx.application.Application;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,68 +13,92 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 import javafx.geometry.Insets;
 import javafx.scene.input.KeyCode;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import javax.swing.*;
 
 public class Main extends Application {
-
-    TextEdit text;
     double WINDOW_WIDTH = 1000;
     double WINDOW_HEIGHT = 500;
+    TextEdit text;
+    int numTimesBoldPressed = 0;
 
-    public HBox addTopMenu(Stage stage) {
+    /**
+     * addTopMenu creates the blue and light blue banners at the top of the word processor and adds buttons to it
+     * @param stage is the stage
+     * @return VBox - the VBox holds the blue and light blue HBox's
+     */
+    public VBox addTopMenu(Stage stage) {
+        VBox vbox = new VBox();
+
         HBox hbox = new HBox();
         hbox.setPadding(new Insets(15, 15, 15, 15));
         hbox.setSpacing(10);
         hbox.setStyle("-fx-background-color: #095996;");
 
+        HBox hBoxLower = new HBox();
+        hBoxLower.setPadding(new Insets(5, 15, 5, 15));
+        hBoxLower.setSpacing(10);
+        hBoxLower.setStyle("-fx-background-color: #d5dfe6;");
+        Text instructions = new Text("Tips:    Ctrl+S = Save,    Ctrl+O = Open (overwrites current file),    Ctrl+Z = Undo");
+        instructions.setFont(Font.font (Font.getDefault().getName(), 12));
+        hBoxLower.getChildren().add(instructions);
+
         Button buttonSave = new Button("Save");
         buttonSave.setPrefSize(70, 20);
         buttonSave.setFocusTraversable(false);
-        buttonSave.setOnAction(event -> {
-            saveFile(stage);
-        });
+        buttonSave.setOnAction(event -> saveFile(stage));
 
         Button buttonOpen = new Button("Open");
         buttonOpen.setPrefSize(70, 20);
         buttonOpen.setFocusTraversable(false);
-        buttonOpen.setOnAction(event -> {
-            openFile(stage);
-        });
+        buttonOpen.setOnAction(event -> openFile(stage));
 
         Button buttonUndo = new Button("Undo");
         buttonUndo.setPrefSize(70, 20);
         buttonUndo.setFocusTraversable(false);
-        buttonUndo.setOnAction(event -> {
-            //TODO: Make this undo
-            text.undo();
-        });
+        buttonUndo.setOnAction(event -> text.undo());
 
-        Button buttonFont = new Button("Font");
-        buttonFont.setPrefSize(70,20);
+        Button buttonFont = new Button("Change Font");
+        buttonFont.setPrefSize(110,20);
         buttonFont.setFocusTraversable(false);
-
-        buttonFont.setOnAction(actionEvent -> {
-            JOptionPane.showMessageDialog(null,"Coming Soon!");
-        });
+        buttonFont.setOnAction(event -> text.changeFont());
 
         Button buttonBold = new Button("Bold");
         buttonBold.setPrefSize(70,20);
         buttonBold.setFocusTraversable(false);
-        buttonBold.setOnAction(actionEvent -> {
-
-            JOptionPane.showMessageDialog(null,"Coming Soon!");
+        buttonBold.setOnAction(event -> {
+            text.changeBold();
+            if (numTimesBoldPressed%2==0) {
+                buttonBold.setText("Un-Bold");
+            } else {
+                buttonBold.setText("Bold");
+            }
+            numTimesBoldPressed++;
         });
 
-        hbox.getChildren().addAll(buttonSave, buttonOpen, buttonUndo, buttonFont, buttonBold);
+        Button buttonClearAll = new Button(("Clear All"));
+        buttonClearAll.setPrefSize(70, 20);
+        buttonClearAll.setFocusTraversable(false);
+        buttonClearAll.setOnAction(event -> text.clearText());
 
-        return hbox;
+        hbox.getChildren().addAll(buttonSave, buttonOpen, buttonUndo, buttonFont, buttonBold, buttonClearAll);
+
+        vbox.getChildren().addAll(hbox,hBoxLower);
+
+        return vbox;
     }
 
+    /**
+     * saveTextToFile method makes a printwriter copy what the user has typed
+     * into a file that can be saved to the computer
+     * @param content receives what the user has written so far
+     * @param file adds to the file specified by user
+     * @throws FileNotFoundException - if file is not found
+     */
     private void saveTextToFile(String content, File file) throws FileNotFoundException {
         PrintWriter writer;
         writer = new PrintWriter(file);
@@ -79,12 +106,37 @@ public class Main extends Application {
         writer.close();
     }
 
+    /**
+     * saveFile method allows the user to use the save button found in the word processor.
+     * It creates the system "save file" dialogue and calls "saveToFile" to copy the user's input
+     * @param stage - the stage
+     */
+    public void saveFile(Stage stage){
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showSaveDialog(stage);
+        if (file != null) {
+            try {
+                saveTextToFile(text.getText(), file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * openFile allows the user to press Open on the word processor and brings up the window's file page.
+     * Uses a scanner to scan through all the text and displays it on the word processor.
+     * @param stage - the stage
+     */
     private void openFile(Stage stage) {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
         fileChooser.getExtensionFilters().add(extFilter);
         File file = fileChooser.showOpenDialog(stage);
         if (file != null) {
+            text.clearText();
             try {
                 Scanner sc = new Scanner(file);
                 StringBuilder sb = new StringBuilder();
@@ -101,23 +153,8 @@ public class Main extends Application {
         }
     }
 
-    public void saveFile(Stage stage){
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-        fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showSaveDialog(stage);
-        if (file != null) {
-            try {
-                saveTextToFile(text.getText(), file);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     /**
-     * Set up the starting scene of your application given the primaryStage (basically the window)
-     *
+     * Sets up the starting scene
      * @param primaryStage the primary container for scenes
      */
     // https://docs.oracle.com/javase/8/javafx/api/index.html
@@ -126,13 +163,12 @@ public class Main extends Application {
         // Add a title to the application window
         primaryStage.setTitle("Untitled.txt");
 
-
         BorderPane border = new BorderPane();
-        Scene exampleScene = new Scene(border, WINDOW_WIDTH, WINDOW_HEIGHT);
-        primaryStage.setScene(exampleScene);
+        Scene scene = new Scene(border, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setScene(scene);
 
         //adds the menu at the top that holds the buttons
-        HBox topMenu = addTopMenu(primaryStage);
+        VBox topMenu = addTopMenu(primaryStage);
         border.setTop(topMenu);
 
         text = new TextEdit(border);
@@ -140,13 +176,10 @@ public class Main extends Application {
         // display the interface
         primaryStage.show();
 
-
         //BUTTON PRESSES
-        exampleScene.setOnKeyTyped(event -> {
+        scene.setOnKeyTyped(event -> {
             if (!event.isControlDown() && (int) event.getCharacter().charAt(0) != 13 && (int) event.getCharacter().charAt(0) != 8){
                 text.addText(event.getCharacter());
-                int a = (int) event.getCharacter().charAt(0);
-//                System.out.println(a);
             }
         });
 
@@ -154,12 +187,7 @@ public class Main extends Application {
         KeyCodeCombination ctrlS = new KeyCodeCombination(KeyCode.S, KeyCodeCombination.CONTROL_DOWN);
         KeyCodeCombination ctrlO = new KeyCodeCombination(KeyCode.O, KeyCodeCombination.CONTROL_DOWN);
 
-        KeyCodeCombination ctrlLeft = new KeyCodeCombination(KeyCode.LEFT, KeyCodeCombination.CONTROL_DOWN);
-        KeyCodeCombination ctrlRight = new KeyCodeCombination(KeyCode.RIGHT, KeyCodeCombination.CONTROL_DOWN);
-        KeyCodeCombination ctrlUp = new KeyCodeCombination(KeyCode.UP, KeyCodeCombination.CONTROL_DOWN);
-        KeyCodeCombination ctrlDown = new KeyCodeCombination(KeyCode.DOWN, KeyCodeCombination.CONTROL_DOWN);
-
-        exampleScene.setOnKeyPressed(event -> {
+        scene.setOnKeyPressed(event -> {
             if (event.getCode().equals(KeyCode.ESCAPE)) {
                 System.exit(0);
             } else if (event.getCode().equals(KeyCode.BACK_SPACE)){
--- src/main/java/com/example/project5_wordprocessor/TextEdit.java
+++ src/main/java/com/example/project5_wordprocessor/TextEdit.java
@@ -1,55 +1,94 @@
 package com.example.project5_wordprocessor;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
-
-import java.util.Deque;
-import java.util.LinkedList;
+import javafx.util.Duration;
+import java.util.*;
 
 public class TextEdit {
     double WINDOW_WIDTH = 1000;
     double WINDOW_HEIGHT = 500;
-    StackPane contentPane;
+    Pane contentPane;
     private final StringBuilder beforeCursor = new StringBuilder();
+    private final StringBuilder blankBeforeCursor = new StringBuilder();
     private final StringBuilder afterCursor = new StringBuilder();
+    private final StringBuilder blankAfterCursor = new StringBuilder();
     private final Text startText;
     private final Text content;
-    private Deque<String> history;
-    private Deque<Character> deletedChars;
-    private Deque<Integer> eventIndex;
+    private final Text cursorText;
+    private final Deque<String> history;
+    private final Deque<Character> deletedChars;
+    private final Deque<Integer> eventIndex;
+    private final List<String> fonts;
+    private String currentFont;
+    private boolean fontIsBold;
 
-    // create a new text node to display text on the interface
-    // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/text/Text.html
+    /**
+     * the TextEdit constructor creates the several text nodes and puts them into a ScrollPane,
+     * which is added to the layout.
+     * It also creates the stacks necessary for undo-ing.
+     *
+     * @param layout - the layout that the text's container will be added to
+     */
     public TextEdit(BorderPane layout) {
         history = new LinkedList<>();
         deletedChars = new LinkedList<>();
         eventIndex = new LinkedList<>();
+        fonts = new ArrayList<>(Arrays.asList("Courier New", "Consolas"));
 
-        contentPane = new StackPane();
+        contentPane = new Pane();
         contentPane.setPadding(new Insets(15, 15, 15, 15));
+        EventHandler<MouseEvent> handler = MouseEvent::consume;
+        contentPane.addEventFilter(MouseEvent.ANY, handler);
+
+        currentFont = "Consolas";
+        fontIsBold = false;
+
+        cursorText = new Text("|");
+        cursorText.setFont(Font.font (currentFont, 18));
+        cursorText.setTextAlignment(TextAlignment.LEFT);
+        cursorText.setWrappingWidth(WINDOW_WIDTH - 100);
+        cursorText.setY(25);
+        cursorText.setX(15);
+
+        double BLINK_INTERVAL = 500;
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(BLINK_INTERVAL), event -> {
+            if (contentPane.getChildren().contains(cursorText)) {
+                contentPane.getChildren().remove(cursorText);
+            } else {
+                contentPane.getChildren().add(cursorText);
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
 
         startText = new Text();
         startText.setText(" Start typing...");
-        startText.setFont(Font.font ("Consolas", 18));
+        startText.setFont(Font.font (currentFont, 18));
         startText.setFill(Color.LIGHTGRAY);
         startText.setTextAlignment(TextAlignment.LEFT);
-        StackPane.setAlignment(startText, Pos.TOP_LEFT);
+        startText.setY(25);
+        startText.setX(15);
 
         content = new Text();
-        content.setText("|");
-        content.setFont(Font.font ("Consolas", 18));
+        content.setFont(Font.font (currentFont, 18));
         content.setTextAlignment(TextAlignment.LEFT);
         content.setWrappingWidth(WINDOW_WIDTH - 100);
-        StackPane.setAlignment(content, Pos.TOP_LEFT);
+        content.setY(25);
+        content.setX(15);
 
         //adds both Text nodes to the contentPane
-        contentPane.getChildren().addAll(content, startText);
+        contentPane.getChildren().addAll(content, startText, cursorText);
 
         //adds the contentPane to a scrollable pane
         ScrollPane scrollPane = new ScrollPane();
@@ -61,34 +100,52 @@ public class TextEdit {
         layout.setLeft(scrollPane);
     }
 
+    /**
+     * Used when saving the document, gets the text the user has typed
+     * @return a string of what the user has typed
+     */
     public String getText(){
         return beforeCursor + afterCursor.toString();
     }
 
+    /**
+     * displays the text
+     */
     public void displayText() {
         if (beforeCursor.isEmpty() && afterCursor.isEmpty()){
             contentPane.getChildren().add(startText);
         } else {
             contentPane.getChildren().remove(startText);
         }
-        content.setText(beforeCursor + "|" + afterCursor);
+        content.setText(beforeCursor + " " + afterCursor);
+        cursorText.setText(blankBeforeCursor + "|" + blankAfterCursor);
 
-//        content.setText(beforeCursor.toString() + afterCursor.toString());
-//        addCursor(pass thru the coordinates at which the beforeCursor string ends, put cursor there)
     }
 
+    /**
+     * adds the text to the StringBuilders
+     * @param s - the text that is being added
+     */
     public void addText(String s){
         if (!s.equals("\b")) {
             beforeCursor.append(s);
-            displayText();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i)==('\n')) {
+                    blankBeforeCursor.append("\n");
+                } else {
+                    blankBeforeCursor.append(" ");
+                }
+                displayText();
+            }
         }
         history.push(s);
         eventIndex.push(beforeCursor.length()-1);
-
-//        System.out.println(history.toString());
-//        System.out.println(eventIndex.toString());
     }
 
+    /**
+     * deletes the character right before the cursor
+     * @param s - the backspace character
+     */
     public void backspace(String s){
         if (!beforeCursor.isEmpty()) {
             deletedChars.push(beforeCursor.charAt(beforeCursor.length()-1));
@@ -96,40 +153,61 @@ public class TextEdit {
             history.push(s);
 
             beforeCursor.deleteCharAt(beforeCursor.length() - 1);
-//            content.setText(beforeCursor.toString());
+            blankBeforeCursor.deleteCharAt(blankBeforeCursor.length() - 1);
+
             displayText();
         }
     }
 
+    /**
+     * moves the cursor one to the left
+     */
     public void moveCursorLeft(){
         if (!beforeCursor.isEmpty()) {
             afterCursor.insert(0, beforeCursor.charAt(beforeCursor.length() - 1));
+            blankAfterCursor.insert(0, blankBeforeCursor.charAt(blankBeforeCursor.length() - 1));
             beforeCursor.deleteCharAt(beforeCursor.length() - 1);
+            blankBeforeCursor.deleteCharAt(blankBeforeCursor.length() - 1);
         }
         displayText();
     }
 
+    /**
+     * moves the cursor one to the right
+     */
     public void moveCursorRight(){
         if (!afterCursor.isEmpty()) {
             beforeCursor.append(afterCursor.charAt(0));
+            blankBeforeCursor.append(blankAfterCursor.charAt(0));
             afterCursor.deleteCharAt(0);
+            blankAfterCursor.deleteCharAt(0);
         }
         displayText();
     }
 
+    /**
+     * this aids in finding the lengths of different parts of what the user has typed.
+     * necessary for moveCursorUp and moveCursorDown.
+     */
     static int nthLastIndexOf(int nth, String ch, String string) {
         if (nth <= 0) return string.length();
         return nthLastIndexOf(--nth, ch, string.substring(0, string.lastIndexOf(ch)));
     }
 
+    /**
+     * moves the cursor up, putting it in the same position but on the line above.
+     * (unless the line above is shorter, then it goes to the end of the above line)
+     */
     public void moveCursorUp(){
 //        System.out.println(beforeCursor.toString());
         if (beforeCursor.toString().contains("\n")){
             int lastLineLength = beforeCursor.toString().length()-beforeCursor.toString().lastIndexOf("\n")-1;
             int secondToLastLineLength = -1+beforeCursor.length()-(beforeCursor.substring(0, nthLastIndexOf(1,"\n",beforeCursor.toString())).lastIndexOf("\n"))-lastLineLength;
             int amtToRemove;
-            if (secondToLastLineLength>=lastLineLength){
+            if (secondToLastLineLength>lastLineLength){
                 amtToRemove = secondToLastLineLength;
+            } else if (secondToLastLineLength==lastLineLength) {
+                amtToRemove = secondToLastLineLength + 1;
             } else {
                 amtToRemove = lastLineLength + 1;
             }
@@ -146,53 +224,127 @@ public class TextEdit {
         }
     }
 
+    /**
+     * moves the cursor down, putting it in the same position but on the line below.
+     * (unless the line above is shorter, then it goes to the end of the below line)
+     */
     public void moveCursorDown(){
-        //if the beforeCursor has a line break:
-            //get num characters in afterCursor before the first line break
-            //move cursor up that many characters in the below line
-        // else:
-            //move cursor to end of afterCursor
-//        if (afterCursor.toString().contains("\n")){
-//            int beforeCursorLastLineLength = beforeCursor.toString().length()-beforeCursor.toString().lastIndexOf("\n")-1;
-//            int currentLineLength = beforeCursorLastLineLength+(afterCursor.indexOf("\n"));
-//
-//            int amtToMove = currentLineLength + 1;
-//
-//            //move cursor right 'currentLineLength' times
-//            for (int i=0; i < amtToMove; i++){
-//                moveCursorRight();
-//            }
-//        } else {
-//            int length = afterCursor.length();
-//            for (int i = 0; i < length; i++){
-//                moveCursorRight();
-//            }
-//        }
+        if (afterCursor.toString().contains("\n")){
+            int nextLineLength = afterCursor.indexOf("\n", afterCursor.indexOf("\n") + 1) - (afterCursor.indexOf("\n")) - 1;
+            if (nextLineLength<0) {
+                nextLineLength = afterCursor.length() - (afterCursor.indexOf("\n")) - 1;
+            }
+
+            int beforeCursorLastLineLength = beforeCursor.toString().length() - beforeCursor.toString().lastIndexOf("\n")-1;
+            int currentLineLength = beforeCursorLastLineLength + (afterCursor.indexOf("\n"));
+            int amtToMove;
+
+            if (beforeCursorLastLineLength<nextLineLength || nextLineLength>currentLineLength){
+                amtToMove = currentLineLength + 1;
+            } else {
+                amtToMove = afterCursor.indexOf("\n") + nextLineLength + 1;
+            }
+
+            for (int i = 0; i < amtToMove; i++){
+                moveCursorRight();
+            }
+
+        } else {
+            int length = afterCursor.length();
+            for (int i = 0; i < length; i++){
+                moveCursorRight();
+            }
+        }
     }
 
+    /**
+     * undo's the last action.
+     * if a character was just deleted, it puts it back.
+     * if a character was just added, it removes it.
+     */
     public void undo(){
         if (!history.isEmpty() && !eventIndex.isEmpty()){
-//            System.out.println(eventIndex.peek());
-//            System.out.println(beforeCursor.length()-1);
                 if (history.peek().equals("\b")) {
-                    beforeCursor.insert((int) eventIndex.poll(), deletedChars.poll());
+                    beforeCursor.insert((int) eventIndex.peek(), deletedChars.peek());
+                    if (deletedChars.peek()=='\n'){
+                        blankBeforeCursor.insert(eventIndex.peek(), "\n");
+                    } else{
+                        blankBeforeCursor.insert(eventIndex.peek(), " ");
+                    }
+                    eventIndex.pop();
+                    deletedChars.pop();
                     history.pop();
                 } else {
                     if (eventIndex.peek() < beforeCursor.length()) {
-                        beforeCursor.deleteCharAt(eventIndex.poll());
-                        history.pop();
+                        beforeCursor.deleteCharAt(eventIndex.peek());
+                        blankBeforeCursor.deleteCharAt(eventIndex.peek());
                     } else {
-                        afterCursor.deleteCharAt(eventIndex.poll() - beforeCursor.length());
-                        history.pop();
+                        afterCursor.deleteCharAt(eventIndex.peek() - beforeCursor.length());
+                        blankAfterCursor.deleteCharAt(eventIndex.peek() - blankBeforeCursor.length());
                     }
+                    eventIndex.pop();
+                    history.pop();
                 }
                 displayText();
         }
     }
 
+    /**
+     * clears the stacks for when the file is reset (either cleared or a new file opened)
+     */
     public void clearStacks(){
         history.clear();
         deletedChars.clear();
         eventIndex.clear();
+    }
+
+    /**
+     * removes all text from the StringBuilders.
+     * All the user's hard work... gone in an instant!
+     * Useful when opening a new file
+     */
+    public void clearText(){
+        if (!beforeCursor.isEmpty()) {
+            beforeCursor.delete(0, beforeCursor.length());
+            blankBeforeCursor.delete(0, blankBeforeCursor.length());
+            afterCursor.delete(0, afterCursor.length());
+            blankAfterCursor.delete(0, blankAfterCursor.length());
+            displayText();
+        }
+    }
+
+    /**
+     *changes the font weight to bold
+     */
+    public void changeBold(){
+        if (!fontIsBold) {
+            content.setFont(Font.font(currentFont, FontWeight.BOLD, 18));
+            cursorText.setFont(Font.font(currentFont, FontWeight.BOLD, 18));
+            fontIsBold = true;
+        } else {
+            content.setFont(Font.font(currentFont, FontWeight.NORMAL, 18));
+            fontIsBold = false;
+        }
+    }
+
+    /**
+     * changes the font to another font in the list
+     */
+    public void changeFont(){
+        System.out.println(currentFont + "   " + fonts.get(0));
+        System.out.println(currentFont.equals(fonts.get(0)));
+        if (currentFont.equals("Consolas")){
+            currentFont = fonts.get(0);
+        } else {
+            currentFont = fonts.get(1);
+        }
+        if (fontIsBold) {
+            content.setFont(Font.font(currentFont, FontWeight.BOLD, 18));
+            cursorText.setFont(Font.font(currentFont, FontWeight.BOLD, 18));
+        } else {
+            content.setFont(Font.font(currentFont, FontWeight.NORMAL, 18));
+            cursorText.setFont(Font.font(currentFont, FontWeight.NORMAL, 18));
+        }
+
     }
 }
